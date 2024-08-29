4175e578b59a6536218a279b498a7ce54d16582d
Michael May <michael.m9267@gmail.com>
2024-08-28 20:19:41 -0500

    added credit to original developer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ function App() {
   return (
     <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
       <CodeEditor />
+      <p>
+        Built by{" "}
+        <a
+          href="https://github.com/nikitadev-yt"
+          style={{ color: "white", textDecoration: "underline" }}
+        >
+          nikitadev-yt
+        </a>
+      </p>
     </Box>
   );
 }
